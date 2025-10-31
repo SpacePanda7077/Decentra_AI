c902e3218dd85bfd5751b74c4276d73141ebbec5
SpacePanda7077 <spacepanda7077@gmail.com>
2025-10-31 05:14:14 -0700

    changes the top up method

--- frontend/src/components/Topup.tsx
+++ frontend/src/components/Topup.tsx
@@ -1,8 +1,14 @@
 import { useState } from "react";
-import { prepareTransaction, sendTransaction, toWei } from "thirdweb";
+import {
+  prepareTransaction,
+  sendTransaction,
+  toWei,
+  waitForReceipt,
+} from "thirdweb";
 import { hederaTestnetConfig } from "@/utils/chains";
 import { client } from "@/lib/thirdweb";
 import { useActiveAccount } from "thirdweb/react";
+import { hackathon_supabase } from "@/lib/supabase";
 
 function TopUp() {
   const account = useActiveAccount();
@@ -11,8 +17,9 @@ function TopUp() {
   const Handle_top_up = async () => {
     if (!account) return;
     if (top_up_amount <= 0) return;
+
     const transaction = prepareTransaction({
-      to: process.env.Top_UP_Wallet_Address,
+      to: "0x00000000000000000000000000000000006d4002",
       value: toWei(top_up_amount.toString()),
       chain: {
         rpc: hederaTestnetConfig.rpcUrls.default.http[0],
@@ -24,6 +31,79 @@ function TopUp() {
       account,
       transaction,
     });
+    const receipt = await waitForReceipt({
+      transactionHash: tx.transactionHash,
+      client: tx.client,
+      chain: tx.chain,
+    });
+    if (receipt) {
+      check_if_auth_and_update(top_up_amount);
+    }
+  };
+
+  const check_if_auth_and_update = async (credit: number) => {
+    if (!account) return;
+    const {
+      data: { user },
+      error,
+    } = await hackathon_supabase.auth.getUser();
+    if (user) {
+      console.log(user);
+      const { data: existingProfile } = await hackathon_supabase
+        .from("user_top_up")
+        .select("id")
+        .eq("id", user.id)
+        .single();
+
+      if (existingProfile) {
+        updateCredit(user);
+      } else {
+        console.log("🆕 You need to sign up");
+      }
+    } else {
+      const { data, error } = await hackathon_supabase.auth.signInWithWeb3({
+        chain: "ethereum",
+      });
+      if (error) {
+        return;
+      }
+      console.log(data);
+      // create a profile row for them
+
+      const { data: InsertData, error: InsertError } = await hackathon_supabase
+        .from("user_top_up")
+        .insert({
+          id: data.user.id,
+          wallet_address: account.address,
+          credits: 0,
+        });
+      if (InsertError) {
+        console.log(InsertError);
+        return;
+      }
+      updateCredit(data.user);
+    }
+  };
+
+  const updateCredit = async (user: { id: string }) => {
+    const { data: selectData, error: selectError } = await hackathon_supabase
+      .from("user_top_up")
+      .select("*")
+      .eq("id", user.id)
+      .single(); // important: ensures it returns one object, not an array
+
+    if (selectError) console.error(selectError);
+    else console.log("User profile:", selectData);
+    console.log("👋 Returning user");
+    const userCredit = selectData.credits;
+    console.log(userCredit);
+    const updateAmount = userCredit + top_up_amount;
+    const { data: updateData, error: updateError } = await hackathon_supabase
+      .from("user_top_up")
+      .update({ credits: updateAmount }) // fields you want to change
+      .eq("id", user.id); // which row(s) to update
+    if (updateError) console.error(updateError);
+    else set_top_up_amount(0);
   };
   return (
     <>
--- frontend/src/lib/supabase.ts
+++ frontend/src/lib/supabase.ts
@@ -3,4 +3,12 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
+const hackathon_supabaseUrl = process.env.HACKATHON_PUBLIC_SUPABASE_URL!;
+const hackathon_supabaseKey = process.env.HACKATHON_PUBLIC_SUPABASE_ANON_KEY!;
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
+
+export const hackathon_supabase = createClient(
+  "https://xzedwlnzjpdykwyvsiac.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6ZWR3bG56anBkeWt3eXZzaWFjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE4MjY2ODAsImV4cCI6MjA3NzQwMjY4MH0.YmRe-FYF6EE78wjYPMuBDCzj7Enf7gGR4upKX51OeHo"
+);
